bad365f9336368de51f17e591a95fa4ef8612439
Fer1014 <u201623453@upc.edu.pe>
2023-11-05 23:55:05 -0500

    Change #1

---
+++ src/app/components/user-registrar/user-registrar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-registrar',
+  templateUrl: './user-registrar.component.html',
+  styleUrls: ['./user-registrar.component.css']
+})
+export class UserRegistrarComponent {
+
+}
