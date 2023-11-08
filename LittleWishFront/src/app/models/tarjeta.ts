bf33ca42d685350ee33e69cbb2832d63fede7746
Fernando Sotelo Ramon <113149667+Fer1014@users.noreply.github.com>
2023-11-07 19:08:43 -0500

    Delete LittleWishFront directory

--- LittleWishFront/src/app/models/tarjeta.ts
+++
@@ -1,8 +0,0 @@
-export class Tarjeta {
-    idTarjeta:number=0
-    numeroTarjeta:number=0
-    fechaexp: Date = new Date(Date.now())
-    nombre:string=""
-    apellido:string=""
-    cvv:number=0
-}
--- LittleWishFront/src/app/models/tipousuario.ts
+++
@@ -1,7 +0,0 @@
-import { Usuarios } from "./usuarios";
-
-export class TipoUsuario {
-    id:number=0
-    rol:string=""
-    users: Usuarios = new Usuarios
-}
